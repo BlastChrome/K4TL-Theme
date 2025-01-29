8458f0f3d32adc8b39be4a05cc09a7cecbd24540
Nicholas Jackson <nicholasjackson5656@gmail.com>
2025-01-29 08:42:25 -0800

    consolidated the update,remove & addParams function into a single updateParam function

--- assets/facets.js
+++ assets/facets.js
@@ -9,7 +9,6 @@ class FacetFiltersForm extends HTMLElement {
     sizeButtons.addEventListener('click', this.handleSizeButtonClick.bind(this));
     // initialize searchParams with the current URL params,
     this.searchParams = new URLSearchParams(window.location.search);
-    this.inputBuffer = [];
     this.timeoutID = undefined;
   }
 
@@ -31,55 +30,38 @@ class FacetFiltersForm extends HTMLElement {
     const name = checkboxInput.name;
     const value = checkboxInput.value;
 
-    isChecked ? this.addParams(name, value) : this.removeParams(name, value);
+    isChecked ? this.updateParams(name, value, 'add') : this.updateParams(name, value, 'delete');
   }
 
-  addParams(name, value) {
-    // determine if the paramater already exists
-    const paramExists = this.searchParams.get(name) ? true : false;
+  updateParams(name, value, action) {
+    if (!action || !name || !value) return;
 
-    if (!paramExists) {
-      // add the new parameter
+    if (action == 'add') {
       this.searchParams.append(name, value);
-    } else {
-      // update existing parameter
+    }
+    if (action == 'delete') {
+      this.searchParams.delete(name, value);
+    }
+    if (action == 'update') {
       this.searchParams.set(name, value);
     }
     window.location.search = this.searchParams.toString();
   }
-  removeParams(name, value) {
-    this.searchParams.delete(name, value);
-    // update and send the url
-    window.location.search = this.searchParams.toString();
-  }
-
-  updateParams(oldName, newName, value) {
-    this.searchParams.get(oldName);
-  }
 
   handlePriceFilter(numInput) {
-    // determine if it's the max or min input
-    const isMAX = numInput.id == 'max-price' ? true : false;
-
-    //determine if the url has
-    console.log(this.searchParams.getAll('filter.v.price.lte'));
-
     // clear the last input timeout
     clearInterval(this.timeoutID);
-
     const name = numInput.name;
-    // get the last input
-    const lastInput = numInput.value.split('').pop();
-
-    // add that input to the buffer
-    this.inputBuffer.push(lastInput);
-
-    //combine the array into a single value, [1,2,5] = $125
-    const result = this.inputBuffer.join('');
+    const value = numInput.value;
 
     //after a certain amount of time update the filters with the result
     this.timeoutID = setTimeout(() => {
-      this.addParams(name, result);
+      const paramExists = this.searchParams.get(name) ? true : false;
+      if (!paramExists) {
+        this.updateParams(name, value, 'update');
+      } else {
+        this.updateParams(name, value, 'add');
+      }
     }, 500);
   }
 
@@ -90,7 +72,7 @@ class FacetFiltersForm extends HTMLElement {
     const name = clickedButton.getAttribute('name');
     const value = clickedButton.getAttribute('value');
 
-    isActive ? this.removeParams(name, value) : this.addParams(name, value);
+    isActive ? this.updateParams(name, value, 'delete') : this.updateParams(name, value, 'add');
   }
 }
 
