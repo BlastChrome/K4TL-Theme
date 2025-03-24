ab3cb5a662e9b3613a160b4449dda04951e69b1d
Nicholas Jackson <nicholasjackson5656@gmail.com>
2025-03-24 13:50:17 -0700

    made the urlsearchparams a static method of the facets class, that way each instance of facet-filters would all be talking to the same global variable, which was the intended behavior

--- assets/facets.js
+++ assets/facets.js
@@ -1,8 +1,9 @@
 class FacetFiltersForm extends HTMLElement {
+  static searchParams = new URLSearchParams(window.location.search);
+
   constructor() {
     super();
     const facetForm = this.querySelector('form');
-    this.searchParams = new URLSearchParams(window.location.search);
     facetForm.addEventListener('click', this.handleFormClickEvents.bind(this));
     facetForm.addEventListener('change', this.handleFormChangeEvent.bind(this));
   }
@@ -16,67 +17,55 @@ class FacetFiltersForm extends HTMLElement {
     if (facetRemoveElement) {
       e.preventDefault();
       const linkToRemove = e.target.closest('a');
-      const name = linkToRemove.dataset.name;
-      const value = linkToRemove.dataset.value;
-      this.updateParams(name, value, 'delete');
+      FacetFiltersForm.updateParams(linkToRemove.dataset.name, linkToRemove.dataset.value, 'delete');
     }
     if (filterClearElement) {
       e.preventDefault();
-      this.updateParams(null, null, 'clear');
+      FacetFiltersForm.updateParams(null, null, 'clear');
     }
     if (sizeButton) {
       e.preventDefault();
-      const isActive = sizeButton.classList.contains('active');
       const name = sizeButton.getAttribute('name');
       const value = sizeButton.getAttribute('value');
-      isActive ? this.updateParams(name, value, 'delete') : this.updateParams(name, value, 'add');
+      const isActive = sizeButton.classList.contains('active');
+      FacetFiltersForm.updateParams(name, value, isActive ? 'delete' : 'add');
     }
     if (clickedInput) {
-      const isChecked = clickedInput.checked;
       const name = clickedInput.name;
       const value = clickedInput.value;
-      isChecked ? this.updateParams(name, value, 'add') : this.updateParams(name, value, 'delete');
+      const isChecked = clickedInput.checked;
+      FacetFiltersForm.updateParams(name, value, isChecked ? 'add' : 'delete');
     }
   }
 
   handleFormChangeEvent(e) {
     const facetSelection = e.target.closest('select');
     if (!facetSelection) return;
-    const value = e.target.value;
-    this.updateParams('sort_by', value, 'add');
+    FacetFiltersForm.updateParams('sort_by', e.target.value, 'add');
   }
 
-  updateParams(name, value, action) {
-    console.log(this.searchParams.toString());
-    debugger;
+  static updateParams(name, value, action) {
     if (!action) return;
-    if (action == 'clear') {
-      this.searchParams.forEach((_, key) => this.searchParams.delete(key)); // Remove all parameters
-      const cleanUrl = '/collections/all';
-      this.updatePageWithFilters(cleanUrl);
+
+    if (action === 'clear') {
+      FacetFiltersForm.searchParams = new URLSearchParams();
+      FacetFiltersForm.updatePageWithFilters('/collections/all');
       return;
     }
-    if (action == 'add') {
-      this.searchParams.append(name, value);
+    if (action === 'add') {
+      FacetFiltersForm.searchParams.append(name, value);
     }
-    if (action == 'delete') {
-      this.searchParams.delete(name, value);
+    if (action === 'delete') {
+      FacetFiltersForm.searchParams.delete(name, value);
     }
-    if (action == 'update') {
-      this.searchParams.set(name, value);
+    if (action === 'update') {
+      FacetFiltersForm.searchParams.set(name, value);
     }
-    this.updatePageWithFilters();
+
+    FacetFiltersForm.updatePageWithFilters();
   }
 
-  async updatePageWithFilters(someUrl) {
-    let url = null;
-    if (!someUrl) {
-      url = `/collections/all?${this.searchParams.toString()}`;
-    } else {
-      url = someUrl;
-    }
-
-    // Use the History API to update the URL in the address bar without reloading the page
+  static async updatePageWithFilters(url = `/collections/all?${FacetFiltersForm.searchParams.toString()}`) {
     window.history.pushState({ path: url }, '', url);
     fetch(url)
       .then((response) => response.text())
@@ -84,23 +73,20 @@ class FacetFiltersForm extends HTMLElement {
         const parser = new DOMParser();
         const html = parser.parseFromString(text, 'text/html');
 
+        // Update product grid
         const productGrid = document.querySelector('.product-slider.product-slider--grid');
-        // get the updated filters and products from the html element
-        const updatedProducts = html.querySelectorAll('.product-card');
         productGrid.innerHTML = '';
-        updatedProducts.forEach((product) => productGrid.appendChild(product));
+        html.querySelectorAll('.product-card').forEach((product) => productGrid.appendChild(product));
 
-        // wrapper element for the mobile/sidebar filter
-        const oldFilterWrappers = document.querySelectorAll('facet-filters-form .wrapper');
-
-        const newSidebarFilters = html.querySelector('facet-filters-form.sidebar-filters .filter--sidebar');
-        const newMobileFilters = html.querySelector('facet-filters-form.mobile-filters .filter-wrapper__content');
-
-        oldFilterWrappers.forEach((wrapper) => {
+        // Update filters
+        document.querySelectorAll('facet-filters-form .wrapper').forEach((wrapper) => {
           wrapper.innerHTML = '';
-          wrapper.classList.contains('mobile-wrapper')
-            ? wrapper.appendChild(newMobileFilters)
-            : wrapper.appendChild(newSidebarFilters);
+          const newFilters = html.querySelector(
+            wrapper.classList.contains('mobile-wrapper')
+              ? 'facet-filters-form.mobile-filters .filter-wrapper__content'
+              : 'facet-filters-form.sidebar-filters .filter--sidebar'
+          );
+          wrapper.appendChild(newFilters);
         });
       });
   }
